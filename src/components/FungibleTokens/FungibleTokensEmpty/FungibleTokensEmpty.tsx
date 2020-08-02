b06a1c821a9d57659ebae24bde83164ba800f7ff
Fabricio Miranda <fabriciomirandabr@gmail.com>
2020-08-02 10:07:48 -0300

    Update

--- src/components/FungibleTokens/FungibleTokensEmpty/FungibleTokensEmpty.tsx
+++ src/components/FungibleTokens/FungibleTokensEmpty/FungibleTokensEmpty.tsx
@@ -1,10 +1,11 @@
 import { Card } from 'antd'
 import React from 'react'
 import ft from '../../../assets/ft.svg'
+import './FungibleTokensEmpty.scss'
 
 export default function FungibleTokensEmpty() {
   return (
-    <Card className='fungible-tokens'>
+    <Card className='fungible-tokens-empty'>
       <img src={ft} alt='Fungible Token' />
       <p>You need to convert some Non-Fungible Token into Fungible Token</p>
     </Card>
--- src/components/NonFungibleTokens/NonFungibleTokensEmpty/NonFungibleTokensEmpty.tsx
+++ src/components/NonFungibleTokens/NonFungibleTokensEmpty/NonFungibleTokensEmpty.tsx
@@ -5,11 +5,11 @@ import './NonFungibleTokensEmpty.scss'
 
 export default function NonFungibleTokensEmpty() {
   return (
-    <Card className='non-fungible-tokens'>
+    <Card className='non-fungible-tokens-empty'>
       <img src={nft} alt='Non Fungible Token' />
       <p>Connect your Ethereum Wallet to add Non-Fungible Tokens</p>
       <div className='disabled-form'>
-        <Input placeholder='Basic usage' disabled />
+        <Input placeholder='NFT Hash' disabled />
         <Button type='primary' disabled>
           Add NFT
         </Button>
