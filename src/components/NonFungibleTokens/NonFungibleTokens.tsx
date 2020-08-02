fef2dfd45510106dae16cadcca2db0cf345b8fdf
Fabricio Miranda <fabriciomirandabr@gmail.com>
2020-08-02 18:38:03 -0300

    Update

--- src/components/NonFungibleTokens/NonFungibleTokens.tsx
+++ src/components/NonFungibleTokens/NonFungibleTokens.tsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
+import { WalletContext } from '../../context/WalletContext'
+import { ERC721Item } from '../../services/api'
 import NonFungibleTokensEmpty from './NonFungibleTokensEmpty/NonFungibleTokensEmpty'
 import NonFungibleTokensList from './NonFungibleTokensList/NonFungibleTokensList'
 
 export default function NonFungibleTokens() {
-  const [nft] = useState<any[]>([])
+  const { wallet, accounts, accountIndex } = useContext(WalletContext)
+
+  const [nft, setNft] = useState<ERC721Item[]>([])
+
+  const loadNft = useCallback(async () => {
+    if (wallet) {
+      const nfts = await wallet.listAccountItems(accounts[accountIndex], 9, 10)
+      setNft(nfts.items)
+    }
+  }, [wallet, accounts, accountIndex])
+
+  useEffect(() => {
+    loadNft()
+  }, [loadNft])
 
   if (!nft.length) {
     return <NonFungibleTokensEmpty />
--- src/components/NonFungibleTokens/NonFungibleTokensEmpty/NonFungibleTokensEmpty.tsx
+++ src/components/NonFungibleTokens/NonFungibleTokensEmpty/NonFungibleTokensEmpty.tsx
@@ -1,16 +1,23 @@
 import { Button, Card, Input } from 'antd'
-import React from 'react'
+import React, { useContext } from 'react'
 import nft from '../../../assets/nft.svg'
+import { WalletContext } from '../../../context/WalletContext'
 import './NonFungibleTokensEmpty.scss'
 
 export default function NonFungibleTokensEmpty() {
+  const { accounts, accountIndex } = useContext(WalletContext)
+
   return (
     <Card className='non-fungible-tokens-empty'>
       <img src={nft} alt='Non Fungible Token' />
-      <p>Connect your Ethereum Wallet to add Non-Fungible Tokens</p>
+      {!accounts[accountIndex] ? (
+        <p>Connect your Ethereum Wallet to add Non-Fungible Tokens</p>
+      ) : (
+        <p>Input your Non Fungible Token Hash on the form and click Add NFT button</p>
+      )}
       <div className='disabled-form'>
-        <Input placeholder='NFT Hash' disabled />
-        <Button type='primary' disabled>
+        <Input placeholder='NFT Hash' disabled={!accounts[accountIndex]} />
+        <Button type='primary' disabled={!accounts[accountIndex]}>
           Add NFT
         </Button>
       </div>
