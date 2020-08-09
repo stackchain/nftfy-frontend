93452ae8b0637cb6853f32501e8d6c2354e569c8
Fabricio Miranda <fabriciomirandabr@gmail.com>
2020-08-09 18:56:52 -0300

    Added Fallback

--- src/components/ContractSecuritization/ContractImage/ContractImage.tsx
+++ src/components/ContractSecuritization/ContractImage/ContractImage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ft from '../../../assets/ft.svg'
 import './ContractImage.scss'
 
 interface Props {
@@ -12,7 +13,7 @@ export default function ContractImage({ name, meta, description, src }: Props) {
   return (
     <div className='contract-image-item'>
       <div>
-        <img src={src.split('https://cors-anywhere.herokuapp.com/')[1] || src} alt={name} />
+        <img src={src.split('https://cors-anywhere.herokuapp.com/')[1] || src || ft} alt={name} />
         <div className='contract-name'>{name}</div>
         <div className='contract-meta'>{meta}</div>
         <div className='contract-description'>{description}</div>
