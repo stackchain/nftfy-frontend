0ab171df9d807d973bd742af8235cc14179e1cb5
Paulo Barcelos <46430156+paulobarcelosbr@users.noreply.github.com>
2021-01-07 20:05:20 -0300

    Add Zoom Support in NFT Images (#4)
    * upgrade packages end ligth box image antd
    
    * finalize task from Add Zoom Support in NFT Images
    
    * comment correction

--- src/components/ContractSecuritization/ContractImage/ContractImage.tsx
+++ src/components/ContractSecuritization/ContractImage/ContractImage.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { Image } from 'antd'
+import 'antd/dist/antd.css'
+import React, { useState } from 'react'
 import nftImage from '../../../assets/nft.svg'
 import './ContractImage.scss'
 
@@ -10,10 +12,17 @@ interface Props {
 }
 
 export default function ContractImage({ name, meta, description, src }: Props) {
+  const [isVisible, setIsVisible] = useState(false)
+
+  function setVisible() {
+    setIsVisible(!isVisible)
+  }
+
   return (
     <div className='contract-image-item'>
       <div>
-        <img src={src.includes('ipfs://') ? nftImage : src || nftImage} alt={name} />
+        <Image width={180} src={src.includes('ipfs://') ? nftImage : src || nftImage} alt={name} onPreviewClose={setVisible} />
+        {isVisible && <div className='title-image-nft'>{name}</div>}
         <div className='contract-name'>{name}</div>
         <div className='contract-meta'>{meta}</div>
         <div className='contract-description'>{description}</div>
