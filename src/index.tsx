983abd45a50b19c8ba2930fa6a01532f0ce6c948
Fabricio Miranda <fabricio@science.clinic>
2021-01-08 09:34:39 -0300

    Added Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -5,10 +7,18 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import './styles.scss'
 
+Sentry.init({
+  dsn: 'https://5f01256df48f493799286d68b54cc0ed@o501737.ingest.sentry.io/5583250',
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+  environment: 'production'
+})
+
 ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 )
 
