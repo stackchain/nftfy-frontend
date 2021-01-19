f11f604d21fe6b70237f785caa0c99b37b76e40d
Fabricio Miranda <fabriciomirandabr@gmail.com>
2021-01-19 09:41:22 -0300

    Added Storybook

---
+++ src/__stories__/ExampleButton.stories.tsx
@@ -0,0 +1,38 @@
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+// eslint-disable-next-line import/no-unresolved
+import { Meta, Story } from '@storybook/react/types-6-0'
+import React from 'react'
+import { ExampleButton, ExampleButtonProps } from '../components/shared/buttons'
+
+export default {
+  title: 'components/shared/buttons/ExampleButton',
+  component: ExampleButton,
+  argTypes: {
+    backgroundColor: { control: 'color' }
+  }
+} as Meta
+
+const Template: Story<ExampleButtonProps> = args => <ExampleButton {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  primary: true,
+  label: 'Button'
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  label: 'Button'
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  size: 'large',
+  label: 'Button'
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  size: 'small',
+  label: 'Button'
+}
