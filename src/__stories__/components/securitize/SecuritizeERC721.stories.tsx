3954f9a88c71bf94d395276ab8cddb69c50a9664
Fabricio Miranda <fabriciomirandabr@gmail.com>
2021-02-16 17:55:00 -0300

    Change Securitize Name

--- src/__stories__/components/securitize/SecuritizeERC721.stories.tsx
+++ src/__stories__/components/securitize/SecuritizeERC721.stories.tsx
@@ -1,10 +1,10 @@
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 // eslint-disable-next-line import/no-unresolved
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { SecuritizeERC721, SecuritizeERC721Props } from '../../../components/collection/SecuritizeERC721'
+import { SecuritizeERC721, SecuritizeERC721Props } from '../../../components/securitize/SecuritizeERC721'
 
 export default {
-  title: 'components/collection/SecuritizeERC721',
+  title: 'components/securitize/SecuritizeERC721',
   component: SecuritizeERC721,
   argTypes: {}
 } as Meta
