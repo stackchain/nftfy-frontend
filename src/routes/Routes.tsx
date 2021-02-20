e7542deae12775d6e455949faeb71b15ea52618f
Fabricio Miranda <fabriciomirandabr@gmail.com>
2021-02-20 15:02:39 -0300

    Removed Lazy Loading

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,5 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
-import { LazyLoading } from '../components/shared/loading'
 import FallbackPage from '../pages/FallbackPage'
 import IntroPage from '../pages/IntroPage'
 import MarketplaceDetailsPage from '../pages/MarketplaceDetailsPage'
@@ -10,19 +9,17 @@ import SecuritizePage from '../pages/SecuritizePage'
 
 export default function Routes() {
   return (
-    <Suspense fallback={<LazyLoading />}>
-      <Switch>
-        <Route path='/' exact component={IntroPage} />
-        <Route path='/marketplace' exact component={MarketplacePage} />
-        <Route path='/marketplace/:address' exact component={MarketplaceDetailsPage} />
-        <Route path='/securitize' exact component={SecuritizePage} />
-        <Route path='/securitize/:address/:tokenId' exact component={FallbackPage} />
-        <Route path='/portfolio' exact component={PortfolioPage} />
-        <Route path='/tutorial' exact component={FallbackPage} />
-        <Route path='/terms' exact component={FallbackPage} />
-        <Route path='/privacy-policy' exact component={FallbackPage} />
-        <Redirect exact from='**' to='/' />
-      </Switch>
-    </Suspense>
+    <Switch>
+      <Route path='/' exact component={IntroPage} />
+      <Route path='/marketplace' exact component={MarketplacePage} />
+      <Route path='/marketplace/:address' exact component={MarketplaceDetailsPage} />
+      <Route path='/securitize' exact component={SecuritizePage} />
+      <Route path='/securitize/:address/:tokenId' exact component={FallbackPage} />
+      <Route path='/portfolio' exact component={PortfolioPage} />
+      <Route path='/tutorial' exact component={FallbackPage} />
+      <Route path='/terms' exact component={FallbackPage} />
+      <Route path='/privacy-policy' exact component={FallbackPage} />
+      <Redirect exact from='**' to='/' />
+    </Switch>
   )
 }
