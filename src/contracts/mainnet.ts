adb8f2a0a31ef2438c0a3e2fe686b87fe09f92cc
Fabricio Miranda <fabriciomirandabr@gmail.com>
2021-02-24 11:25:53 -0300

    Added Config File

--- src/contracts/mainnet.ts
+++
@@ -1,7 +0,0 @@
-export const addressInfuraMainnet = `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`
-
-export const addressNfyMainnet = '0xc633BAf9fDE99800226C74328024525192294D2b'
-
-export const addressNftfyMainnet = ''
-
-export const addressesERC721Mainnet = []
